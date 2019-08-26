af177941555c3e5c9e3bf3b62b95816087391717
Kevin <43301648+pyropotato@users.noreply.github.com>
2019-08-26 15:41:17 +0530

    Update think_write.py

--- think_write.py
+++ think_write.py
@@ -1,18 +1,18 @@
 import os
 import time
 import urllib3   #https://urllib3.readthedocs.io/en/latest/
-import Adafruit_DHT
 http = urllib3.PoolManager()
+KEY = "SA72C8CVDX16H3HB"
 while True:
 	try:
-		hum,temp = Adafruit_DHT.read_retry(11,4)
-		#print(hum," humidity")
-		#print(temp," temp")
-		baseURL = "https://api.thingspeak.com/update?api_key=HC5BRM3YICTI19D1"
-		url = baseURL+"&field1=%f,&field2=%f" %(temp,hum)
-		#print(url)
+		f1 = 50
+		f2 = 50
+		baseURL = "https://api.thingspeak.com/update?api_key=%s" %(KEY)
+		url = baseURL+"&field1=%f,&field2=%f" %(f1,f2)
+		print(url)
 		response = http.request('GET',url)
 		print (response.data)
 		time.sleep(1)
-	except:
-		pass
+	except KeyboardInterrupt:
+		print("Stopped")
+		break
