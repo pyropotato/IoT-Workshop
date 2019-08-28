9316f53d2700eb0d9f55f02e826f496d4f4e70fa
Kevin <43301648+pyropotato@users.noreply.github.com>
2019-08-28 15:17:48 +0530

    Update mqtt_subs.py

--- mqtt_subs.py
+++ mqtt_subs.py
@@ -6,12 +6,13 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("core/tests")
+    client.subscribe(topic)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+topic = "home/room"
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
