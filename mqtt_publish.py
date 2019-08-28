bdeb69150b2541e3e0af3e237f650bc69f34822e
Kevin <43301648+pyropotato@users.noreply.github.com>
2019-08-28 15:16:43 +0530

    Update mqtt_publish.py

--- mqtt_publish.py
+++ mqtt_publish.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt #import the client1
 broker_address="test.mosquitto.org"
+topic = "home/room"
 client = mqtt.Client("P1") #create new instance
 client.connect(broker_address,1883,60) #connect to broker
-client.publish("jack/tests","fuk u")#publish
+client.publish(topic,"test msg")#publish
 client.disconnect()
