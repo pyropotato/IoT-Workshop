11519254f4af79ab972f7266f1a37fdf8bd7a207
Kevin <43301648+pyropotato@users.noreply.github.com>
2019-08-30 08:59:27 +0530

    Update think_write.py

--- think_write.py
+++ think_write.py
@@ -3,16 +3,18 @@ import time
 import urllib3   #https://urllib3.readthedocs.io/en/latest/
 http = urllib3.PoolManager()
 KEY = "SA72C8CVDX16H3HB"
+f1 = 50
+f2 = 50
 while True:
 	try:
-		f1 = 50
-		f2 = 50
 		baseURL = "https://api.thingspeak.com/update?api_key=%s" %(KEY)
 		url = baseURL+"&field1=%f,&field2=%f" %(f1,f2)
 		print(url)
 		response = http.request('GET',url)
 		print (response.data)
 		time.sleep(1)
+		f1 = f1 + 10
+		f2 = f2 + 5
 	except KeyboardInterrupt:
 		print("Stopped")
 		break
